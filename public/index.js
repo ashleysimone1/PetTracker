d9cffc31dc8b0948f5fac20ce4b869a12fa83283
ashleysimone1 <ashtucker1204@gmail.com>
2023-12-13 14:37:59 -0500

    created a post app

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
-const petForm = document.querySelector()
+const petForm = document.querySelector('#pet-form');
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const petObj = Object.fromEntries(new FormData(e.target));
+    petObj.friendly = petObj.friendly === "on";
+    console.log(petObj);
+
+    fetch('/api', () => {
+        method: 'POST'
+    })
+}
+
+petForm.addEventListener('submit', handleSubmit);
--- src/server.js
+++ src/server.js
@@ -3,11 +3,25 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const PORT = 8000;
+const Pets  = require('./db/models/pets')
 
 const publicDir = path.join(__dirname, '..', 'public');
 
 const staticAssets = express.static(publicDir);
 app.use(staticAssets);
+
+app.get('/allPets', async (req, res) => {
+    const petList = await Pets.list()
+    console.log(petList);
+    res.send(petList);
+});
+
+app.post('/api', async (req, res) => {
+    console.log(req.body);
+    const { friendly, petName, petPicture, petSpecies } = req.body;
+    const newPet = await Pets.create(req.body);
+    res.send()
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port on http://localhost:${PORT}`);
